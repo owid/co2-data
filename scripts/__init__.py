bb1cbddc1fa5f84f24cfc3b27b171f2393678cff
Pablo Rosado <pablo.rosado@inetum.altia.es>
2022-02-23 15:16:19 +0100

    style: remove unused path

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,5 +2,4 @@ import os
 
 CURRENT_DIR = os.path.dirname(__file__)
 INPUT_DIR = os.path.join(CURRENT_DIR, "input")
-GRAPHER_DIR = os.path.join(CURRENT_DIR, "grapher")
 OUTPUT_DIR = os.path.join(CURRENT_DIR, "..")
