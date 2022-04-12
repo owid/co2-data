eba78acb300df4520d6dd9e5ea4e859293c28385
Pablo Rosado <pabloarosado@gmail.com>
2022-04-12 15:20:15 +0200

    enhance(main): replace variable ids with dictionary of names and ids

---
+++ scripts/input/variable_ids.py
@@ -0,0 +1,81 @@
+"""Define variable names and their corresponding variable ids.
+
+"""
+
+co2_emissions_ids = {
+  "Annual CO2 emissions": 179841,
+  "Annual CO2 emissions (per capita)": 179842,
+  "Annual CO2 emissions embedded in trade": 179845,
+  "Annual CO2 emissions from cement": 179847,
+  "Annual CO2 emissions from cement (per capita)": 179848,
+  "Annual CO2 emissions from coal": 179851,
+  "Annual CO2 emissions from coal (per capita)": 179852,
+  "Annual CO2 emissions from flaring": 179855,
+  "Annual CO2 emissions from flaring (per capita)": 179856,
+  "Annual CO2 emissions from gas": 179859,
+  "Annual CO2 emissions from gas (per capita)": 179860,
+  "Annual CO2 emissions from oil": 179863,
+  "Annual CO2 emissions from oil (per capita)": 179864,
+  "Annual CO2 emissions from other industry": 179867,
+  "Annual CO2 emissions from other industry (per capita)": 179868,
+  "Annual CO2 emissions growth (%)": 179871,
+  "Annual CO2 emissions growth (abs)": 179872,
+  "Annual CO2 emissions per GDP (kg per $PPP)": 179873,
+  "Annual CO2 emissions per unit energy (kg per kilowatt-hour)": 179874,
+  "Annual consumption-based CO2 emissions": 179875,
+  "Annual consumption-based CO2 emissions (per capita)": 179876,
+  "Annual consumption-based CO2 emissions per GDP (kg per $PPP)": 179877,
+  "Cumulative CO2 emissions": 179878,
+  "Cumulative CO2 emissions from cement": 179880,
+  "Cumulative CO2 emissions from coal": 179882,
+  "Cumulative CO2 emissions from flaring": 179884,
+  "Cumulative CO2 emissions from gas": 179886,
+  "Cumulative CO2 emissions from oil": 179888,
+  "Cumulative CO2 emissions from other industry": 179890,
+  "Share of annual CO2 emissions embedded in trade": 179892,
+  "Share of global annual CO2 emissions": 179893,
+  "Share of global annual CO2 emissions from cement": 179894,
+  "Share of global annual CO2 emissions from coal": 179895,
+  "Share of global annual CO2 emissions from flaring": 179896,
+  "Share of global annual CO2 emissions from gas": 179897,
+  "Share of global annual CO2 emissions from oil": 179898,
+  "Share of global annual CO2 emissions from other industry": 179899,
+  "Share of global cumulative CO2 emissions": 179900,
+  "Share of global cumulative CO2 emissions from cement": 179901,
+  "Share of global cumulative CO2 emissions from coal": 179902,
+  "Share of global cumulative CO2 emissions from flaring": 179903,
+  "Share of global cumulative CO2 emissions from gas": 179904,
+  "Share of global cumulative CO2 emissions from oil": 179905,
+  "Share of global cumulative CO2 emissions from other industry": 179906,
+}
+
+total_ghg_emissions_ids = {
+  'Total including LUCF': 347635,
+  'Total including LUCF (per capita)': 347636,
+}
+
+ch4_emissions_ids = {
+  'Total including LUCF': 347573,
+  'Total including LUCF (per capita)': 347574,
+  'Total excluding LUCF': 347571,
+  'Total excluding LUCF (per capita)': 347572,
+}
+
+n2o_emissions_ids = {
+  'Total including LUCF': 347617,
+  'Total including LUCF (per capita)': 347618,
+}
+
+gdp_ids = {
+  'GDP': 417484,
+}
+
+population_ids = {
+  'Population (historical estimates)': 72,
+}
+
+primary_energy_consumption_ids = {
+  'Primary energy consumption (TWh)': 143360,
+  'Energy consumption per capita (kWh)': 143363,
+  'Energy consumption per GDP (kWh per $)': 143364,
+}
