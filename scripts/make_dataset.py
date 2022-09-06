9a803cfcd18e7d8c21fd3035a6ce80af706e3924
Pablo Rosado <pabloarosado@gmail.com>
2022-09-06 16:53:42 +0200

    style: Improve format

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -21,7 +21,9 @@ OUTPUT_JSON_FILE = OUTPUT_DIR / "owid-co2-data.json"
 CODEBOOK_FILE = OUTPUT_DIR / "owid-co2-codebook.csv"
 
 
-def df_to_json(complete_dataset: pd.DataFrame, output_path: str, static_columns: List[str]) -> None:
+def df_to_json(
+    complete_dataset: pd.DataFrame, output_path: str, static_columns: List[str]
+) -> None:
     megajson = {}
 
     for _, row in complete_dataset.iterrows():
@@ -46,7 +48,10 @@ def prepare_data(table: catalog.Table) -> pd.DataFrame:
     # Sort rows and columns conveniently.
     df = df.sort_values(["country", "year"]).reset_index(drop=True)
     first_columns = ["country", "year", "iso_code", "population", "gdp"]
-    df = df[first_columns + [column for column in sorted(df.columns) if column not in first_columns]]
+    df = df[
+        first_columns
+        + [column for column in sorted(df.columns) if column not in first_columns]
+    ]
 
     return df
 
@@ -59,11 +64,18 @@ def prepare_codebook(table: catalog.Table) -> pd.DataFrame:
     table["country"].metadata.sources = [catalog.meta.Source(name="Our World in Data")]
     table["year"].metadata.description = "Year of observation."
     table["year"].metadata.sources = [catalog.meta.Source(name="Our World in Data")]
-    table["iso_code"].metadata.description = "ISO 3166-1 alpha-3, three-letter country codes."
-    table["iso_code"].metadata.sources = [catalog.meta.Source(name="International Organization for Standardization")]
+    table[
+        "iso_code"
+    ].metadata.description = "ISO 3166-1 alpha-3, three-letter country codes."
+    table["iso_code"].metadata.sources = [
+        catalog.meta.Source(name="International Organization for Standardization")
+    ]
     table["population"].metadata.description = "Population of each country or region."
-    table["population"].metadata.sources = [catalog.meta.Source(
-        name="Our World in Data based on different sources (https://ourworldindata.org/population-sources)")]
+    table["population"].metadata.sources = [
+        catalog.meta.Source(
+            name="Our World in Data based on different sources (https://ourworldindata.org/population-sources)"
+        )
+    ]
 
     # Gather column names, descriptions and sources from the variables' metadata.
     metadata = {"column": [], "description": [], "source": []}
@@ -76,7 +88,9 @@ def prepare_codebook(table: catalog.Table) -> pd.DataFrame:
     codebook = pd.DataFrame(metadata).set_index("column").sort_index()
     # For clarity, ensure column descriptions are in the same order as the columns in the data.
     first_columns = ["country", "year", "iso_code", "population", "gdp"]
-    codebook = pd.concat([codebook.loc[first_columns], codebook.drop(first_columns)]).reset_index()
+    codebook = pd.concat(
+        [codebook.loc[first_columns], codebook.drop(first_columns)]
+    ).reset_index()
 
     return codebook
 
@@ -86,8 +100,12 @@ def main() -> None:
     # Load data.
     #
     # Load latest OWID-CO2 dataset from the catalog.
-    table = catalog.find("owid_co2", namespace="emissions", channels=["garden"]).\
-        sort_values("version", ascending=False).iloc[0].load()
+    table = (
+        catalog.find("owid_co2", namespace="emissions", channels=["garden"])
+        .sort_values("version", ascending=False)
+        .iloc[0]
+        .load()
+    )
 
     #
     # Process data.
@@ -106,8 +124,8 @@ def main() -> None:
     # Save outputs.
     #
     # Save dataset to files in different formats.
-    df.to_csv(OUTPUT_CSV_FILE, index=False, float_format='%.3f')
-    df.to_excel(OUTPUT_EXCEL_FILE, index=False, float_format='%.3f')
+    df.to_csv(OUTPUT_CSV_FILE, index=False, float_format="%.3f")
+    df.to_excel(OUTPUT_EXCEL_FILE, index=False, float_format="%.3f")
     df_to_json(df, OUTPUT_JSON_FILE, ["iso_code"])
 
     # Save codebook file.
