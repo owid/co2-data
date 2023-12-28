45dfda10cd68ae52ac8f03f54a163e98f4968b3b
Pablo Rosado <pabloarosado@gmail.com>
2023-12-28 11:46:24 +0100

    Update main script to generate json file more efficiently

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -23,24 +23,34 @@ OUTPUT_JSON_FILE = OUTPUT_DIR / "owid-co2-data.json"
 CODEBOOK_FILE = OUTPUT_DIR / "owid-co2-codebook.csv"
 
 
-def df_to_json(
-    complete_dataset: pd.DataFrame, output_path: str, static_columns: List[str]
-) -> None:
-    megajson = {}
+def save_data_to_json(tb: Table, output_path: str) -> None:
+    tb = tb.copy()
 
-    for _, row in tqdm(complete_dataset.iterrows(), total=len(complete_dataset)):
-        row_country = row["country"]
-        row_dict_static = row.drop("country")[static_columns].dropna().to_dict()
-        row_dict_dynamic = row.drop("country").drop(static_columns).dropna().to_dict()
+    # Initialize output dictionary, that contains one item per country in the data.
+    output_dict = {}
 
-        if row_country not in megajson:
-            megajson[row_country] = row_dict_static
-            megajson[row_country]["data"] = [row_dict_dynamic]
-        else:
-            megajson[row_country]["data"].append(row_dict_dynamic)
+    # Each country contains a dictionary, which contains:
+    # * "iso_code", which is the ISO code (as a string), if it exists.
+    # * "data", which is a list of dictionaries, one per year.
+    #   Each dictionary contains "year" as the first item, followed by all other non-nan indicator values for that year.
+    for country in sorted(set(tb["country"])):
+        # Initialize output dictionary for current country.
+        output_dict[country] = {}
 
+        # If there is an ISO code for this country, add it as a new item of the dictionary.
+        iso_code = tb[tb["country"]==country].iloc[0]["iso_code"]
+        if not pd.isna(iso_code):
+            output_dict[country]["iso_code"] = iso_code
+
+        # Create the data dictionary for this country.
+        dict_country = tb[tb["country"] == country].drop(columns=["country", "iso_code"]).to_dict(orient="records")
+        # Remove all nans.
+        data_country = [{indicator:value for indicator, value in d_year.items() if not pd.isna(value)} for d_year in dict_country]
+        output_dict[country]["data"] = data_country
+
+    # Write dictionary to file as a big json object.
     with open(output_path, "w") as file:
-        file.write(json.dumps(megajson, indent=4))
+        file.write(json.dumps(output_dict, indent=4))
 
 
 def prepare_data(tb: Table) -> Table:
@@ -142,25 +152,34 @@ def prepare_codebook(tb: Table) -> pd.DataFrame:
     return codebook
 
 
+def load_latest_dataset(dataset_name: str = "owid_co2", namespace: str="emissions",
+                        path_to_local_catalog: str = "../etl/data/", channel:str = "garden") -> Table:
+    try:
+        # First try to load the latest OWID energy dataset from the local catalog, if it exists.
+        tables = (
+            LocalCatalog(path_to_local_catalog, channels=[channel])
+            .find(dataset_name, namespace=namespace)
+            .sort_values("version", ascending=False)
+        )
+    except ValueError:
+        # Load the latest OWID energy dataset from the remote catalog.
+        tables = find(
+            dataset_name, namespace=namespace, channels=[channel]
+        ).sort_values("version", ascending=False)
+    table_selected = tables.iloc[0]
+    tb = table_selected.load()
+    print(f"Loaded: {table_selected.path}")
+
+    return tb
+
+
 def main() -> None:
     #
     # Load data.
     #
-    try:
-        # First try to load the latest OWID CO2 dataset from the local catalog, if it exists.
-        # NOTE: If the latest dataset exists but is not found, run "reindex" from the etl poetry shell.
-        tables = (
-            LocalCatalog("../etl/data/", channels=["garden"])
-            .find("owid_co2", namespace="emissions")
-            .sort_values("version", ascending=False)
-        )
-    except ValueError:
-        # Load the latest OWID-CO2 dataset from the remote catalog.
-        tables = find(
-            "owid_co2", namespace="emissions", channels=["garden"]
-        ).sort_values("version", ascending=False)
-    print(f"Loading owid_co2 version {tables.iloc[0].version}.")
-    tb = tables.iloc[0].load()
+    # Load latest dataset from etl (from a local or otherwise a remote catalog).
+    # NOTE: If the latest dataset exists but is not found, run "reindex" from the etl poetry shell.
+    tb = load_latest_dataset()
 
     #
     # Process data.
@@ -178,11 +197,15 @@ def main() -> None:
     #
     # Save outputs.
     #
-    # Save dataset to files in different formats.
+    # Save data to a csv file.
+    # NOTE: First convert to dataframe to avoid saving metadata as an additional json file.
     pd.DataFrame(tb).to_csv(OUTPUT_CSV_FILE, index=False, float_format="%.3f")
-    pd.DataFrame(tb).to_excel(OUTPUT_EXCEL_FILE, index=False, float_format="%.3f")
-    df_to_json(pd.DataFrame(tb), OUTPUT_JSON_FILE, ["iso_code"])
-
+    # Save data and codebook to an excel file.
+    with pd.ExcelWriter(OUTPUT_EXCEL_FILE) as writer:
+        tb.to_excel(writer, sheet_name='Data', index=False, float_format="%.3f")
+        codebook.to_excel(writer, sheet_name='Metadata')
+    # Save data to json.
+    save_data_to_json(tb, OUTPUT_JSON_FILE)
     # Save codebook file.
     codebook.to_csv(CODEBOOK_FILE, index=False)
 
